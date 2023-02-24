dcf1fabf741a0822a64e8fd2c8922a03889d3c00
Christ A <christjerold007@gmail.com>
2023-02-24 14:00:05 +0530

    first commit

---
+++ ClassTeacher/Includes/footer.php
@@ -0,0 +1,8 @@
+<footer class="sticky-footer bg-white">
+        <div class="container my-auto">
+          <div class="copyright text-center my-auto">
+            <span> &copy; <script> document.write(new Date().getFullYear()); </script> - Developed by Christ Jerold A
+            </span>
+          </div>
+        </div>
+      </footer>
